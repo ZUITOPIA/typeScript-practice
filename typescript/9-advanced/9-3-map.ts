1420b01fa37d3908d234ad53559d0b7440ab6a8d
고주희 <zuitopia@zuitopia.local>
2022-08-23 14:55:26 +0900

    map type 공부

--- typescript/9-advanced/9-3-map.ts
+++ typescript/9-advanced/9-3-map.ts
@@ -1,20 +1,35 @@
-type Video = {
-  title: string;
-  author: string;
-  description: string;
-};
+{
+  type Video = {
+    title: string;
+    author: string;
+    description: string;
+  };
+  // 배열에서의 map 함수
+  // [1, 2].map(item => item * item); // [1, 4]
 
-type VideoOptional = {
-  title?: string;
-  author?: string;
-  description?: string;
-};
+  type Optional<T> = {
+    [P in keyof T]?: T[P]; // for .. in 과 같은 느낌
+  };
 
-type VideoReadOnly = {
-  readonly title: string;
-  readonly author: string;
-  readonly description: string;
-};
+  type VideoOptional = Optional<Video>;
+  const videoOp: VideoOptional = {
+    title: "hello world", // 모든 정보가 optional 이기 때문에 있어도 되고 없어도 됨
+    // 대신 Video에 들어있는 정보 (title, author,description) 가 아닌 다른 정보는 추가할 수 없음!!!
+  };
 
-// 위의 예시 : 매번 새로운 타입을 만들때마다 복붙해주어야하고 새로 만들어야 하는 번거로움이 있는 상황
-// 이를 해결해주는 mapped type !! 알아보기
+  // Map type : 재사용성 높임 (제네릭과 비슷함)
+  type Animal = {
+    name: string;
+    age: number;
+  };
+
+  const animal: Optional<Animal> = {
+    name: "바보",
+    age: 100,
+  };
+
+  // readonly
+  type ReadOnly<T> = {
+    readonly [P in keyof T]?: T[P]; // readonly 는 추후에 성질 변경 불가능
+  };
+}
